fac1d8510c93f4bbb6f00559d48b6bb28d6d52f4
Kristian Skov Johansen <kris701kj@gmail.com>
2024-04-25 17:08:31 +0200

    Readme update

--- MetaActionGenerators/CandidateGenerators/PreconditionPermutationReductionMetaActions.cs
+++ MetaActionGenerators/CandidateGenerators/PreconditionPermutationReductionMetaActions.cs
@@ -1,14 +1,8 @@
-﻿using PDDLSharp.Models.PDDL.Domain;
+﻿using PDDLSharp.Models.PDDL;
+using PDDLSharp.Models.PDDL.Domain;
 using PDDLSharp.Models.PDDL.Expressions;
-using PDDLSharp.Models.PDDL.Problem;
-using PDDLSharp.Models.PDDL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PDDLSharp.CodeGenerators;
 using PDDLSharp.Models.PDDL.Overloads;
+using PDDLSharp.Models.PDDL.Problem;
 
 namespace MetaActionGenerators.CandidateGenerators
 {
