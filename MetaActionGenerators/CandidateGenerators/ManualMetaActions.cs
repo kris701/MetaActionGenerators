ab4e2725828375cc820198703c2dc38518bc6460
Kristian Skov Johansen <kris701kj@gmail.com>
2024-04-29 18:21:11 +0200

    Added a simple "generator" that just outputs meta actions from a path of `.pddl` files

---
+++ MetaActionGenerators/CandidateGenerators/ManualMetaActions.cs
@@ -0,0 +1,36 @@
+﻿using MetaActionGenerators.ArgumentSystem;
+using PDDLSharp.ErrorListeners;
+using PDDLSharp.Models.PDDL.Domain;
+using PDDLSharp.Models.PDDL.Problem;
+using PDDLSharp.Parsers.PDDL;
+
+namespace MetaActionGenerators.CandidateGenerators
+{
+    /// <summary>
+    /// A simple "generator" that simply outputs some manually given meta actions from a path.
+    /// </summary>
+    public class ManualMetaActions : BaseCandidateGenerator
+    {
+        public ManualMetaActions(Dictionary<string, string> generatorArgs, DomainDecl domain, List<ProblemDecl> problems) : base(domain, problems)
+        {
+            Args = new ArgsHandler(new List<Arg>()
+            {
+                new Arg("metaPath", "A path to the folder containing the meta actions.")
+            }, generatorArgs);
+        }
+
+        internal override List<ActionDecl> GenerateCandidatesInner()
+        {
+            if (!Directory.Exists(Args.GetArgument<string>("metaPath")))
+                throw new DirectoryNotFoundException($"Meta action directory not found: {Args.GetArgument<string>("metaPath")}");
+            var candidates = new List<ActionDecl>();
+
+            var listener = new ErrorListener();
+            var parser = new PDDLParser(listener);
+            foreach (var file in Directory.GetFiles(Args.GetArgument<string>("metaPath")))
+                candidates.Add(parser.ParseAs<ActionDecl>(new FileInfo(file)));
+
+            return candidates;
+        }
+    }
+}
--- MetaActionGenerators/MetaGeneratorBuilder.cs
+++ MetaActionGenerators/MetaGeneratorBuilder.cs
@@ -7,7 +7,7 @@ namespace MetaActionGenerators
 {
     public static class MetaGeneratorBuilder
     {
-        public enum GeneratorOptions { CPDDLMutexed, Flipped, Predicate, Stripped, PDDLSharpMacrosReduction, PreconditionPermutationReduction }
+        public enum GeneratorOptions { CPDDLMutexed, Flipped, Predicate, Stripped, PDDLSharpMacrosReduction, PreconditionPermutationReduction, Manual }
         private static readonly Dictionary<GeneratorOptions, Func<DomainDecl, List<ProblemDecl>, Dictionary<string, string>, ICandidateGenerator>> _dict = new Dictionary<GeneratorOptions, Func<DomainDecl, List<ProblemDecl>, Dictionary<string, string>, ICandidateGenerator>>()
         {
             { GeneratorOptions.CPDDLMutexed, (d, p, a) => new CPDDLMutexedMetaActions(a, d, p) },
@@ -16,8 +16,10 @@ namespace MetaActionGenerators
             { GeneratorOptions.Stripped, (d, p, a) => new StrippedMetaActions(d, p) },
             { GeneratorOptions.PDDLSharpMacrosReduction, (d, p, a) => new PDDLSharpMacroReductionMetaActions(a, d, p) },
             { GeneratorOptions.PreconditionPermutationReduction, (d, p, a) => new PreconditionPermutationReductionMetaActions(d, p) },
+            { GeneratorOptions.Manual, (d, p, a) => new ManualMetaActions(a, d, p) },
         };
 
         public static ICandidateGenerator GetGenerator(GeneratorOptions opt, DomainDecl domain, List<ProblemDecl> problems, Dictionary<string, string> args) => _dict[opt](domain, problems, args);
+        public static ICandidateGenerator GetGenerator(GeneratorOptions opt, DomainDecl domain, List<ProblemDecl> problems) => _dict[opt](domain, problems, new Dictionary<string, string>());
     }
 }
